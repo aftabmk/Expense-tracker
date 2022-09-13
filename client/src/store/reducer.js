34c34f6efe5f33b1e08b3e9e4aa782838b8b0bb9
aftabmk <kizhisery@gmail.com>
2022-09-13 21:36:41 +0530

    new file:   .gitignore 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/postcss.config.js 	new file:   client/public/index.html 	new file:   client/public/logo.ico 	new file:   client/public/logo.png 	new file:   client/public/robots.txt 	new file:   client/src/App.css 	new file:   client/src/App.js 	new file:   client/src/components/Form.js 	new file:   client/src/components/Graph.js 	new file:   client/src/components/Labels.js 	new file:   client/src/components/List.js 	new file:   client/src/helper/helper.js 	new file:   client/src/index.css 	new file:   client/src/index.js 	new file:   client/src/store/apiSlice.js 	new file:   client/src/store/reducer.js 	new file:   client/src/store/store.js 	new file:   client/tailwind.config.js 	new file:   server/controller/controller.js 	new file:   server/db/connection.js 	new file:   server/models/model.js 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/routes/route.js 	new file:   server/server.js

---
+++ client/src/store/reducer.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    categories : [],
+    transaction: []
+}
+
+export const expenseSlice = createSlice({
+    name: 'expense',
+    initialState,
+    reducers : {}
+})
+
+export default expenseSlice.reducer;
